bb8286c367ddb10318bea9f990e4340b0d023852
nathanprows77 <66317477+nathanprows77@users.noreply.github.com>
2020-09-01 21:39:42 -0400

    update jupyter

--- salary_vs_performance.sql
+++ salary_vs_performance.sql
@@ -1,9 +1,22 @@
 -- Create Two Tables
 CREATE TABLE salary (
-	'State' TEXT PRIMARY KEY,
-	2016-17 INT
+	"State" TEXT PRIMARY KEY,
+	"2016-2017 Salary" INT
+	);
+DROP TABLE performance
+CREATE TABLE performance (
+	"State" TEXT PRIMARY KEY,
+	"At or Above Basic" INT,
+	"At or Above Proficient" INT
 	);
 	
-CREATE TABLE performance (
-	Jurisdiction TEXT PRIMARY KEY,
-	);
+	-- Query to check successful load
+SELECT * FROM salary;
+
+SELECT * FROM performance;
+
+-- Join tables on county_id
+SELECT salary."2016-2017 Salary", performance."At or Above Basic", performance."At or Above Proficient" 
+FROM performance
+INNER JOIN salary
+ON performance."State" = salary."State";
